4b73d20cde6b481184e30d9adf712b14c8d422ac
YJ_disco <jiaoyijia668@gmail.com>
2025-01-06 20:45:33 +0800

    修正suoyou

---
+++ .vitepress/creators.ts
@@ -0,0 +1,40 @@
+export interface SocialEntry {
+  type: 'github' | 'twitter' | 'email'
+  icon: string
+  link: string
+}
+
+export interface Creator {
+  avatar: string
+  name: string
+  username?: string
+  title?: string
+  org?: string
+  desc?: string
+  links?: SocialEntry[]
+  nameAliases?: string[]
+  emailAliases?: string[]
+}
+
+const getAvatarUrl = (name: string) => `https://github.com/${name}.png`
+
+export const creators: Creator[] = [
+  {
+    name: 'YJ',
+    avatar: '',
+    username: 'ferifering',
+    title: '在写笔记喵',
+    desc: '开发者，后端开发',
+    links: [
+      { type: 'github', icon: 'github', link: 'https://github.com/ferifering' },
+    ],
+    nameAliases: ['YJ', 'YJ_disco'],
+    emailAliases: ['368443427@qq.com'],
+  },
+].map<Creator>((c) => {
+  c.avatar = c.avatar || getAvatarUrl(c.username)
+  return c as Creator
+})
+
+export const creatorNames = creators.map(c => c.name)
+export const creatorUsernames = creators.map(c => c.username || '')
