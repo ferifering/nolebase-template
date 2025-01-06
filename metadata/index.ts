8f2a9e5553035ef7c76818ae7c924d88c1b0fdb2
YJ_disco <jiaoyijia668@gmail.com>
2025-01-07 00:15:55 +0800

    修正7

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,7 +1,7 @@
 /** 文本 */
 export const siteName = 'YJ的知识库'
 export const siteShortName = 'YJ的知识库'
-export const siteDescription = '记录回忆，知识和畅想的地方'
+export const siteDescription = '极致源于梦想，追求源于热爱'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
